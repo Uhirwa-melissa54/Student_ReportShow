000cd8ea5ff1f164192a40d8ad643824a39f348a
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-01-08 21:55:34 +0200

    added students to dictionary

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Welcome to StudentReportShow")
+print("How many student do you have?")
+numberOfStudent=int(input())
+student={}
+for x in range(numberOfStudent):
+    print("Enter Student's Name")
+    name=input()
+    print("Enter Physic's Marks")
+    physMarks=int(input())
+    print("Enter English's Marks")
+    engMarks=int(input())
+    print("Enter Math's Marks")
+    mathMarks=int(input())
+    print("Enter Java's Marks")
+    javaMarks=int(input())
+    print("Enter DSA's Marks")
+    dsaMarks=int(input())
+    student[name]=[physMarks,engMarks,mathMarks,javaMarks]
+else:
+    print("You have entered all Students");
+print("Choose to continue")
+print("1.Continue]\n 0.exit")
+decision=int(input())
+if decision==1:
+    print("Choose what you want to do:")    
